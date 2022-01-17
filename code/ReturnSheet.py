585ad63b0de873927910359fb727d28080657101
suraj <desaisuraj1@gmail.com>
2022-01-17 16:33:07 +0530

    finalized create return sheet

--- code/ReturnSheet.py
+++ code/ReturnSheet.py
@@ -21,12 +21,24 @@ class ReturnSheetWindow(baseClass):
         super(ReturnSheetWindow, self).__init__()
         uic.loadUi(gui_file, self)
 
-        self.return_type = ['rto', 'customer return', 'wrong customer return', 'wrong courier return', 'cancel']
+        # self.return_data = ['rto', 'customer return', 'wrong customer return', 'wrong courier return', 'cancel']
+        self.return_data = {'rto': 0,
+                            'customer return': 0,
+                            'wrong customer return': 0,
+                            'wrong courier return': 0,
+                            'cancel': 0}
         self.return_list = [['Tracking id', 'Status']]
 
         self.return_type_input.setDisabled(True)
         self.record_button.clicked.connect(self.record_next)
         self.save_return_sheet_button.clicked.connect(self.save_return_sheet)
+
+        return_data_string = f"Rto: {self.return_data.get('rto')}\n" \
+                             f"Customer Return: {self.return_data.get('customer return')}\n" \
+                             f"Wrong Courier Return: {self.return_data.get('wrong courier return')}\n" \
+                             f"Wrong Customer Return: {self.return_data.get('wrong customer return')}\n" \
+                             f"Cancel: {self.return_data.get('cancel')}\n"
+        self.return_data_output.setPlainText(return_data_string)
         self.show()
 
     def record_next(self):
@@ -34,29 +46,26 @@ class ReturnSheetWindow(baseClass):
             user_input = self.user_input.text()
             status = self.return_type_input.text()
 
-            if user_input.lower() in self.return_type:
+            if user_input.lower() in self.return_data:
                 self.return_type_input.setText(user_input)
 
-            if status != "" and user_input.lower() not in self.return_type:
+            if status != "" and user_input.lower() not in self.return_data:
+                # add to return list
                 self.return_list.append([user_input, status])
-                self.recorded_detail()
-                print(self.return_list)
+
+                # add to return count
+                return_count = self.return_data.get(status)
+                self.return_data.update({status: return_count + 1})
 
             self.user_input.clear()
-        except Exception as e:
-            print(e)
 
-    def recorded_detail(self):
-        try:
-            courier_return= 0
-            customer_return = 0
-            wrong_courier_return = 0
-            wrong_customer_return = 0
-            cancel = 0
-
-            print(self.return_list)
-
-
+            # update text field
+            return_data_string = f"Rto: {self.return_data.get('rto')}\n" \
+                                 f"Customer Return: {self.return_data.get('customer return')}\n" \
+                                 f"Wrong Courier Return: {self.return_data.get('wrong courier return')}\n" \
+                                 f"Wrong Customer Return: {self.return_data.get('wrong customer return')}\n" \
+                                 f"Cancel: {self.return_data.get('cancel')}\n"
+            self.return_data_output.setPlainText(return_data_string)
         except Exception as e:
             print(e)
 
--- code/testcode.py
+++ code/testcode.py
@@ -1,3 +1,12 @@
-import platform
-a=platform.architecture()
-print(a)
+return_list = [['Tracking id', 'Status'], ['asdf', 'rto'], ['asdf', 'rto'],['asdf', 'rto'],['asdf', 'rto'],
+               ['asdf', 'customer return']]
+return_type = ['rto', 'customer return', 'wrong customer return', 'wrong courier return', 'cancel']
+total_return = {'rto':0,
+                'customer return': 0}
+for list in return_list:
+    if list[1] in return_type:
+        a = total_return.get(list[1])
+        total_return.update({list[1]: a + 1})
+
+
+print(total_return)
