211329a32ae666c657beee64325f94402fff69aa
suraj <desaisuraj1@gmail.com>
2022-01-18 15:38:05 +0530

    corrected flipkart url

--- code/OrderWebScrap.py
+++ code/OrderWebScrap.py
@@ -92,7 +92,8 @@ class OrderScrapWindow(baseclass):
                   f'last Row: {last_row}'
                   f'Portal: {self.portal_selected}')
             data_string = get_json_string(self.portal_selected, ws, start_row, last_row)
-            self.save_file = create_text_file(data_string.get('web_scrap_string'), self.folder_path, f'{self.portal_selected} web scrap')
+            self.save_file = create_text_file(data_string.get('web_scrap_string'), self.folder_path,
+                                              f'{self.portal_selected} web scrap')
             print(self.save_file)
             self.open_folder_button.setDisabled(False)
 
@@ -116,22 +117,23 @@ class OrderScrapWindow(baseclass):
         except Exception as e:
             print(e)
 
+
 # MAIN FUNCTIONS
 def scrap_amazon():
     return {
-    'head' : r'{"_id":"amazon_price","startUrl":',
-    'url' : r'https://sellercentral.amazon.in/orders-v3/order/',
-    'tail' : r""","selectors":[{"delay":0,"id":"order_id","multiple":false,"parentSelectors":["_root"],"regex":"","selector":".a-span12 span.a-text-bold","type":"SelectorText"},{"delay":0,"id":"sku","multiple":false,"parentSelectors":["orderDetaiBox"],"regex":"","selector":"div.product-name-column-word-wrap-break-all:nth-of-type(3) div","type":"SelectorText"},{"delay":0,"id":"rate","multiple":false,"parentSelectors":["orderDetaiBox"],"regex":"","selector":"td.a-text-right span","type":"SelectorText"},{"delay":0,"id":"orderDetaiBox","multiple":true,"parentSelectors":["_root"],"selector":".a-spacing-large tbody tr","type":"SelectorElement"},{"delay":0,"id":"buyer_name","multiple":false,"parentSelectors":["_root"],"regex":"","selector":"span div > span:nth-of-type(1)","type":"SelectorText"},{"delay":0,"id":"gst_state","multiple":false,"parentSelectors":["_root"],"regex":"","selector":"span span:nth-of-type(5)","type":"SelectorText"}]}"""
+        'head': r'{"_id":"amazon_price","startUrl":',
+        'url': r'https://sellercentral.amazon.in/orders-v3/order/',
+        'tail': r""","selectors":[{"delay":0,"id":"order_id","multiple":false,"parentSelectors":["_root"],"regex":"","selector":".a-span12 span.a-text-bold","type":"SelectorText"},{"delay":0,"id":"sku","multiple":false,"parentSelectors":["orderDetaiBox"],"regex":"","selector":"div.product-name-column-word-wrap-break-all:nth-of-type(3) div","type":"SelectorText"},{"delay":0,"id":"rate","multiple":false,"parentSelectors":["orderDetaiBox"],"regex":"","selector":"td.a-text-right span","type":"SelectorText"},{"delay":0,"id":"orderDetaiBox","multiple":true,"parentSelectors":["_root"],"selector":".a-spacing-large tbody tr","type":"SelectorElement"},{"delay":0,"id":"buyer_name","multiple":false,"parentSelectors":["_root"],"regex":"","selector":"span div > span:nth-of-type(1)","type":"SelectorText"},{"delay":0,"id":"gst_state","multiple":false,"parentSelectors":["_root"],"regex":"","selector":"span span:nth-of-type(5)","type":"SelectorText"}]}"""
     }
 
 
 def scrap_flipkart():
-    return{
-    'head' : r'{"_id":"fk_order_with_click_delete","startUrl":',
-    'url' : u"""https://seller.flipkart.com/index.html#dashboard/my-orders?serviceProfile=seller-fulfilled&shipmentType=easy-ship&orderState=shipments_delivered&orderItemId= """,
-    'tail' : r""","selectors":[{"id":"click","parentSelectors":["_root"],"type":"SelectorElementClick","clickElementSelector":"td.clickable","clickElementUniquenessType":"uniqueText","clickType":"clickOnce","delay":2000,"discardInitialElements":"do-not-discard","multiple":false,"selector":"td.clickable"},{"id":"name","parentSelectors":["_root"],"type":"SelectorText","selector":"div.styles__ItemWrapperVertical-sc-9qxfse-1:nth-of-type(2) div:nth-of-type(2)","multiple":false,"delay":0,"regex":""},{"id":"state","parentSelectors":["_root"],"type":"SelectorText","selector":".styles__ItemWrapperVertical-sc-9qxfse-1 div:nth-of-type(6)","multiple":false,"delay":0,"regex":""},{"id":"box","parentSelectors":["_root"],"type":"SelectorElement","selector":"div.styles__OrderItemContainer-sc-9qxfse-6","multiple":true,"delay":0},{"id":"orderid","parentSelectors":["box"],"type":"SelectorText","selector":"div.styles__ItemDetails-sc-1bxatwx-6:nth-of-type(2) div.styles__Value-sc-1bxatwx-14","multiple":false,"delay":0,"regex":""},{"id":"sku","parentSelectors":["box"],"type":"SelectorText","selector":"div.styles__ItemDetails-sc-1bxatwx-6:nth-of-type(1) div.styles__Value-sc-1bxatwx-14","multiple":false,"delay":0,"regex":""},{"id":"rate","parentSelectors":["box"],"type":"SelectorText","selector":"div:nth-of-type(1) div:nth-of-type(6) div.styles__Value-sc-1bxatwx-14","multiple":false,"delay":0,"regex":""}]}"""
+    return {
+        'head': r'{"_id":"fk_order_with_click_delete","startUrl":',
+        'url': u"""https://seller.flipkart.com/index.html#dashboard/my-orders?serviceProfile=seller-fulfilled&shipmentType=easy-ship&orderState=shipments_delivered&orderItemId=""",
+        'tail': r""","selectors":[{"id":"click","parentSelectors":["_root"],"type":"SelectorElementClick","clickElementSelector":"td.clickable","clickElementUniquenessType":"uniqueText","clickType":"clickOnce","delay":2000,"discardInitialElements":"do-not-discard","multiple":false,"selector":"td.clickable"},{"id":"name","parentSelectors":["_root"],"type":"SelectorText","selector":"div.styles__ItemWrapperVertical-sc-9qxfse-1:nth-of-type(2) div:nth-of-type(2)","multiple":false,"delay":0,"regex":""},{"id":"state","parentSelectors":["_root"],"type":"SelectorText","selector":".styles__ItemWrapperVertical-sc-9qxfse-1 div:nth-of-type(6)","multiple":false,"delay":0,"regex":""},{"id":"box","parentSelectors":["_root"],"type":"SelectorElement","selector":"div.styles__OrderItemContainer-sc-9qxfse-6","multiple":true,"delay":0},{"id":"orderid","parentSelectors":["box"],"type":"SelectorText","selector":"div.styles__ItemDetails-sc-1bxatwx-6:nth-of-type(2) div.styles__Value-sc-1bxatwx-14","multiple":false,"delay":0,"regex":""},{"id":"sku","parentSelectors":["box"],"type":"SelectorText","selector":"div.styles__ItemDetails-sc-1bxatwx-6:nth-of-type(1) div.styles__Value-sc-1bxatwx-14","multiple":false,"delay":0,"regex":""},{"id":"rate","parentSelectors":["box"],"type":"SelectorText","selector":"div:nth-of-type(1) div:nth-of-type(6) div.styles__Value-sc-1bxatwx-14","multiple":false,"delay":0,"regex":""}]}"""
 
-        }
+    }
 
 
 def get_json_string(portal_name, ws, start_row, last_row):
@@ -151,9 +153,10 @@ def get_json_string(portal_name, ws, start_row, last_row):
     for row in ws.iter_rows(values_only=True, min_row=start_row, max_row=last_row):
         mid.append(f'{url}{row[0]}')
 
-    return{
+    return {
         'web_scrap_string': f'{head}{dumps(mid)}{tail}'
     }
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
