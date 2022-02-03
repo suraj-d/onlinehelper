0ded026a67ba71d887215ef47692cfbf766cf4bd
suraj <desaisuraj1@gmail.com>
2022-02-03 13:02:28 +0530

    read sheet name, get code in text box

--- code/OrderWebScrap.py
+++ code/OrderWebScrap.py
@@ -1,11 +1,11 @@
+from datetime import datetime
 import sys
-from os import path, startfile
-from subprocess import Popen
+from os import path
 
 from PyQt5.QtWidgets import QFileDialog, QApplication
 from pyperclip import copy
 
-from code.CommanFunction import read_excel_sheet, create_text_file
+from code.CommanFunction import read_excel_sheet, get_excel_sheet_name
 from json import dumps
 from PyQt5 import uic
 from PyQt5.QtCore import QSettings
@@ -24,12 +24,12 @@ class OrderScrapWindow(baseclass):
         uic.loadUi(gui_file, self)
 
         # function value
-        self.sheet_name_input.setText("Sheet1")
+        # self.sheet_name_input.setText("Sheet1")
 
         # Disable button
         self.browse_button.setDisabled(True)
         self.read_excel_button.setDisabled(True)
-        self.genereate_code_button.setDisabled(True)
+        self.generate_code_button.setDisabled(True)
 
         # button connection
         combo_box_item = ["Select Company", "Amazon Order Detail", "Flipkart Order Detail",
@@ -38,13 +38,10 @@ class OrderScrapWindow(baseclass):
         self.scrap_type_combo_box.currentIndexChanged.connect(self.selectionchange)  # get value on selection change
 
         self.excel_sheet_input.textChanged.connect(self.selectionchange)
-        self.sheet_name_input.textChanged.connect(self.selectionchange)
-        self.start_row_input.textChanged.connect(self.selectionchange)
-        self.last_row_input.textChanged.connect(self.selectionchange)
 
         self.browse_button.clicked.connect(self.browse_files)
         self.read_excel_button.clicked.connect(self.read_excel)
-        self.genereate_code_button.clicked.connect(self.to_scrap_txt)
+        self.generate_code_button.clicked.connect(self.to_scrap_txt)
         self.copy_code_button.clicked.connect(self.copy_text)
 
         self.show()
@@ -58,10 +55,11 @@ class OrderScrapWindow(baseclass):
                 self.browse_button.setDisabled(False)
                 if self.excel_sheet_input.text() is not None:
                     self.read_excel_button.setDisabled(False)
+                    self.sheet_name()
             else:
                 self.browse_button.setDisabled(True)
                 self.read_excel_button.setDisabled(True)
-                self.genereate_code_button.setDisabled(True)
+                self.generate_code_button.setDisabled(True)
 
         except Exception as e:
             print(e)
@@ -78,17 +76,27 @@ class OrderScrapWindow(baseclass):
                 self.scrap_type_combo_box.setDisabled(True)
                 file_path = path.normpath(filepath[0])  # backslash in folder path text
                 self.excel_sheet_input.setText(file_path)
+
+                # load sheet name in combo box
+                self.sheet_name()
         except Exception as e:
             print(e)
+
+    def sheet_name(self):
+        excel_file = self.excel_sheet_input.text()
+        sheet_name = get_excel_sheet_name(excel_file).get('sheet_name')
+        self.sheet_name_input.clear()
+        self.sheet_name_input.addItems(sheet_name)
 
     def read_excel(self):
         try:
             excel_sheet_path = self.excel_sheet_input.text()
-            sheet_name = self.sheet_name_input.text()
+            sheet_name = self.sheet_name_input.currentText()
             excel_data = read_excel_sheet(excel_file_path=excel_sheet_path, sheet_name=sheet_name)
             if 'error' not in excel_data:
-                self.start_row_input.setText(str(excel_data.get('start_row')))
+                self.start_row_input.setText(str(1))
                 self.last_row_input.setText(str(excel_data.get('last_row')))
+                self.generate_code_button.setDisabled(False)
         except Exception as e:
             print(e)
 
@@ -96,7 +104,7 @@ class OrderScrapWindow(baseclass):
         try:
             portal_selected = self.scrap_type_combo_box.currentText()
             excel_sheet_path = self.excel_sheet_input.text()
-            sheet_name = self.sheet_name_input.text()
+            sheet_name = self.sheet_name_input.currentText()
             excel_data = read_excel_sheet(excel_sheet_path, sheet_name)
             ws = excel_data.get('ws')
 
@@ -124,45 +132,24 @@ class OrderScrapWindow(baseclass):
 
 
 # MAIN FUNCTIONS
-# def scrap_amazon_order():
-#     return {
-#         'head': r'{"_id":"amazon_price","startUrl":',
-#         'url': u'https://sellercentral.amazon.in/orders-v3/order/',
-#         'tail': r',"selectors":[{"delay":0,"id":"order_id","multiple":false,"parentSelectors":["_root"],"regex":"","selector":".a-span12 span.a-text-bold","type":"SelectorText"},{"delay":0,"id":"sku","multiple":false,"parentSelectors":["orderDetaiBox"],"regex":"","selector":"div.product-name-column-word-wrap-break-all:nth-of-type(3) div","type":"SelectorText"},{"delay":0,"id":"rate","multiple":false,"parentSelectors":["orderDetaiBox"],"regex":"","selector":"td.a-text-right span","type":"SelectorText"},{"delay":0,"id":"orderDetaiBox","multiple":true,"parentSelectors":["_root"],"selector":".a-spacing-large tbody tr","type":"SelectorElement"},{"delay":0,"id":"buyer_name","multiple":false,"parentSelectors":["_root"],"regex":"","selector":"span div > span:nth-of-type(1)","type":"SelectorText"},{"delay":0,"id":"gst_state","multiple":false,"parentSelectors":["_root"],"regex":"","selector":"span span:nth-of-type(5)","type":"SelectorText"}]}'
-#     }
-#
-#
-# def scrap_flipkart_order():
-#     return {
-#         'head': r'{"_id":"fk_order_with_click_delete","startUrl":',
-#         'url': u'https://seller.flipkart.com/index.html#dashboard/my-orders?serviceProfile=seller-fulfilled&shipmentType=easy-ship&orderState=shipments_delivered&orderItemId=',
-#         'tail': r',"selectors":[{"clickElementSelector":"td.clickable","clickElementUniquenessType":"uniqueText","clickType":"clickOnce","delay":2000,"discardInitialElements":"do-not-discard","id":"click","multiple":false,"parentSelectors":["_root"],"selector":"td.clickable","type":"SelectorElementClick"},{"delay":0,"id":"name","multiple":false,"parentSelectors":["_root"],"regex":"","selector":"div.styles__ItemWrapperVertical-sc-9qxfse-1:nth-of-type(2) div:nth-of-type(2)","type":"SelectorText"},{"delay":0,"id":"state","multiple":false,"parentSelectors":["_root"],"regex":"","selector":".styles__ItemWrapperVertical-sc-9qxfse-1 div:nth-of-type(6)","type":"SelectorText"},{"delay":0,"id":"box","multiple":true,"parentSelectors":["_root"],"selector":"div.styles__OrderItemContainer-sc-9qxfse-6","type":"SelectorElement"},{"delay":0,"id":"orderid","multiple":false,"parentSelectors":["box"],"regex":"","selector":"div.styles__ItemDetails-sc-1bxatwx-6:nth-of-type(2) div.styles__Value-sc-1bxatwx-14","type":"SelectorText"},{"delay":0,"id":"sku","multiple":false,"parentSelectors":["box"],"regex":"","selector":"div.styles__ItemDetails-sc-1bxatwx-6:nth-of-type(1) div.styles__Value-sc-1bxatwx-14","type":"SelectorText"},{"delay":0,"id":"rate","multiple":false,"parentSelectors":["box"],"regex":"","selector":"div:nth-of-type(1) div:nth-of-type(6) div.styles__Value-sc-1bxatwx-14","type":"SelectorText"},{"delay":0,"id":"qty","multiple":false,"parentSelectors":["box"],"regex":"","selector":"div:nth-of-type(1) div.styles__PriceParams-sc-1bxatwx-11:nth-of-type(2) div.styles__Value-sc-1bxatwx-14","type":"SelectorText"}]}'
-#     }
-#
-#
-# def scrap_flipkart_payment():
-#     return {
-#         'head': r'{"_id":"fk_payment_rec_rate","startUrl":',
-#         'url': u'https://seller.flipkart.com/index.html#dashboard/payments/transactions?filter=',
-#         'tail': r',"selectors":[{"delay":0,"id":"order_id","multiple":false,"parentSelectors":["_root"],"regex":"","selector":".cf-list td:nth-of-type(1)","type":"SelectorText"},{"delay":0,"id":"payment","multiple":false,"parentSelectors":["_root"],"regex":"","selector":"#blinx-wrapper-38 span.total-amount","type":"SelectorText"}]}'
-#     }
-
+date = datetime.now().strftime("%Y-%m-%d %H%M")
 
 scrap_code = {'Amazon Order Detail': {
-    'head': r'{"_id":"amazon_price","startUrl":',
+    'head': '{"_id":"' + date + ' amazon_detail","startUrl":',
     'url': u'https://sellercentral.amazon.in/orders-v3/order/',
     'tail': r',"selectors":[{"delay":0,"id":"order_id","multiple":false,"parentSelectors":["_root"],"regex":"","selector":".a-span12 span.a-text-bold","type":"SelectorText"},{"delay":0,"id":"sku","multiple":false,"parentSelectors":["orderDetaiBox"],"regex":"","selector":"div.product-name-column-word-wrap-break-all:nth-of-type(3) div","type":"SelectorText"},{"delay":0,"id":"rate","multiple":false,"parentSelectors":["orderDetaiBox"],"regex":"","selector":"td.a-text-right span","type":"SelectorText"},{"delay":0,"id":"orderDetaiBox","multiple":true,"parentSelectors":["_root"],"selector":".a-spacing-large tbody tr","type":"SelectorElement"},{"delay":0,"id":"buyer_name","multiple":false,"parentSelectors":["_root"],"regex":"","selector":"span div > span:nth-of-type(1)","type":"SelectorText"},{"delay":0,"id":"gst_state","multiple":false,"parentSelectors":["_root"],"regex":"","selector":"span span:nth-of-type(5)","type":"SelectorText"}]}'
 },
     'Flipkart Order Detail': {
-        'head': r'{"_id":"fk_order_with_click_delete","startUrl":',
+        'head': '{"_id":"' + date + ' flipkart_detail","startUrl":',
         'url': u'https://seller.flipkart.com/index.html#dashboard/my-orders?serviceProfile=seller-fulfilled&shipmentType=easy-ship&orderState=shipments_delivered&orderItemId=',
         'tail': r',"selectors":[{"clickElementSelector":"td.clickable","clickElementUniquenessType":"uniqueText","clickType":"clickOnce","delay":2000,"discardInitialElements":"do-not-discard","id":"click","multiple":false,"parentSelectors":["_root"],"selector":"td.clickable","type":"SelectorElementClick"},{"delay":0,"id":"name","multiple":false,"parentSelectors":["_root"],"regex":"","selector":"div.styles__ItemWrapperVertical-sc-9qxfse-1:nth-of-type(2) div:nth-of-type(2)","type":"SelectorText"},{"delay":0,"id":"state","multiple":false,"parentSelectors":["_root"],"regex":"","selector":".styles__ItemWrapperVertical-sc-9qxfse-1 div:nth-of-type(6)","type":"SelectorText"},{"delay":0,"id":"box","multiple":true,"parentSelectors":["_root"],"selector":"div.styles__OrderItemContainer-sc-9qxfse-6","type":"SelectorElement"},{"delay":0,"id":"orderid","multiple":false,"parentSelectors":["box"],"regex":"","selector":"div.styles__ItemDetails-sc-1bxatwx-6:nth-of-type(2) div.styles__Value-sc-1bxatwx-14","type":"SelectorText"},{"delay":0,"id":"sku","multiple":false,"parentSelectors":["box"],"regex":"","selector":"div.styles__ItemDetails-sc-1bxatwx-6:nth-of-type(1) div.styles__Value-sc-1bxatwx-14","type":"SelectorText"},{"delay":0,"id":"rate","multiple":false,"parentSelectors":["box"],"regex":"","selector":"div:nth-of-type(1) div:nth-of-type(6) div.styles__Value-sc-1bxatwx-14","type":"SelectorText"},{"delay":0,"id":"qty","multiple":false,"parentSelectors":["box"],"regex":"","selector":"div:nth-of-type(1) div.styles__PriceParams-sc-1bxatwx-11:nth-of-type(2) div.styles__Value-sc-1bxatwx-14","type":"SelectorText"}]}'
     },
     'Flipkart Payment Detail': {
-        'head': r'{"_id":"fk_payment_rec_rate","startUrl":',
+        'head': '{"_id":"' + date + ' flipkart_payment","startUrl":',
         'url': u'https://seller.flipkart.com/index.html#dashboard/payments/transactions?filter=',
-        'tail': r',"selectors":[{"delay":0,"id":"order_id","multiple":false,"parentSelectors":["_root"],"regex":"","selector":".cf-list td:nth-of-type(1)","type":"SelectorText"},{"delay":0,"id":"payment","multiple":false,"parentSelectors":["_root"],"regex":"","selector":"#blinx-wrapper-38 span.total-amount","type":"SelectorText"}]}'
-    }}
+        'tail': r',"selectors":[{"delay":0,"id":"order_id","multiple":false,"parentSelectors":["_root"],"regex":"","selector":".cf-list td:nth-of-type(1)","type":"SelectorText"},{"delay":0,"id":"payment","multiple":false,"parentSelectors":["_root"],"regex":"","selector":"span.total-amount","type":"SelectorText"}]}'
+    }
+}
 
 
 def get_web_scrap_json_string(portal_name, ws, start_row, last_row):
