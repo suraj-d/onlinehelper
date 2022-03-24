e325e943d4e6663ce2445e9f61bc6cb6bc7aa35f
suraj <desaisuraj1@gmail.com>
2022-03-24 18:39:48 +0530

    added reminder to save file if not saved

--- src/ReturnSheet.py
+++ src/ReturnSheet.py
@@ -4,7 +4,7 @@ from PyQt5 import uic
 from PyQt5.QtCore import Qt, QSettings
 from PyQt5.QtWidgets import QApplication, QFileDialog
 
-from code.CommanFunction import create_xlsx_file
+from src.CommanFunction import create_xlsx_file
 
 if __name__ == "__main__":
     gui_file = "../gui/createReturnSheet.ui"
@@ -22,12 +22,13 @@ class ReturnSheetWindow(baseClass):
 
         # self.return_data = ['rto', 'customer return', 'wrong customer return', 'wrong courier return', 'cancel']
         self.return_data_count = {'rto': 0,
-                            'customer return': 0,
-                            'wrong customer return': 0,
-                            'wrong courier return': 0,
-                            'cancel': 0}
+                                  'customer return': 0,
+                                  'wrong customer return': 0,
+                                  'wrong courier return': 0,
+                                  'cancel': 0}
         self.return_data_list = [['Tracking id', 'Status']]
         self.return_type_scan = None
+        self.file_saved = False
 
         self.return_type_input.setDisabled(True)
         self.record_button.clicked.connect(self.record_next)
@@ -81,19 +82,20 @@ class ReturnSheetWindow(baseClass):
                                  f"Total Return: {total}"
             self.return_data_output.setPlainText(return_data_string)
         except Exception as e:
-            print(e)
+            self.return_data_output.setPlainText(str(e))
 
     def save_return_sheet(self):
         try:
             setting_last_file_path = QSettings("Order Helper", "LastSetting")  # path in regedit
 
             last_open_file_path = setting_last_file_path.value("return_sheet_path")  # get value
-            filepath = QFileDialog.getSaveFileName(self, "Save Return Scan Sheet",last_open_file_path,
+            filepath = QFileDialog.getSaveFileName(self, "Save Return Scan Sheet", last_open_file_path,
                                                    "Excel Files (*.xlsx)")
             sheet_name = "Return Scan"  # sheet name and workbook name
             if filepath[0] != "":
                 create_xlsx_file(self.return_data_list, filepath[0], sheet_name)
                 setting_last_file_path.setValue("return_sheet_path", filepath[0])
+                self.file_saved = True
         except Exception as e:
             print(e)
 
@@ -101,6 +103,14 @@ class ReturnSheetWindow(baseClass):
         try:
             if event.key() in (Qt.Key_Return, Qt.Key_Enter):
                 self.record_next()
+        except Exception as e:
+            print(e)
+
+    def closeEvent(self, event):
+        try:
+            for key, value in self.return_data_count.items():
+                if value > 0 and not self.file_saved:
+                    self.save_return_sheet()
         except Exception as e:
             print(e)
 
